48082d17eb6ed405357faa161c11ab097c5544c3
juancaruizc <juancruizcruz@gmail.com>
2021-02-16 16:08:09 -0700

    file setup, database setup, migration setup, seed setup

---
+++ api/cars/carRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const db = require('../../data/dbConfig');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+module.exports = router;
